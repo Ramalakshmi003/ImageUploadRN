7d260a9829af6d33a90bb826e455f09c2ba1bbb8
Ramalakshmi <ramalakshmi.esakki@dreamguystech.com>
2023-05-30 09:32:46 +0530

    bug cleared in launchCamera and implemented date and time picker

--- src/screens/PersonalDetails.js
+++ src/screens/PersonalDetails.js
@@ -5,6 +5,7 @@ import React, { useState, useContext } from 'react'
 import ModalDropdown from 'react-native-modal-dropdown'
 import { CustomBtn, ButtonText } from '../components/commonStyled'
 import { MyContext } from '../components/component/MyContext'
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 
 export const naviBtn = () => {
@@ -31,12 +32,52 @@ const PersonalDetails = ({ navigation }) => {
     //     setSelectedValue(item.value);
     // };
 
-    const {fName,setFName,lName,setLName,gender,setGender,DOB,setDOB,cardNum,setCardNum} = useContext(MyContext)
+    const { fName, setFName, lName, setLName, gender, setGender, DOB, setDOB, cardNum, setCardNum } = useContext(MyContext)
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+    const [selectedDate, setSelectedDate] = useState('Select Date');
+    const [selectTime, setSelectTime] = useState('Select Time');
 
     const genderOptions = ['Female', 'Male', 'Not Specified'];
     const onSelectGender = (index, value) => {
         setGender(value);
     };
+
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+    };
+
+    const handleDateConfirm = (date) => {
+        console.warn("A date has been picked: ", date);
+        const dt = new Date(date);
+        const x = dt.toISOString().split('T');
+        const x1 = x[0].split('-');
+        console.log(x1[2] + "/" + x1[1] + "/" + x1[0])
+        setSelectedDate(x1[2] + "/" + x1[1] + "/" + x1[0])
+        hideDatePicker();
+    };
+
+    const showTimePicker = () => {
+        setTimePickerVisibility(true);
+    };
+
+    const hideTimePicker = () => {
+        setTimePickerVisibility(false);
+    };
+
+    const handleTimeConfirm = (date) => {
+        console.warn("A date has been picked: ", date);
+        const dt = new Date(date);
+        const x = dt.toLocaleTimeString();
+        console.log(x)
+        setSelectTime(x);
+        hideTimePicker();
+    };
+
     // const [selectedValue, setSelectedValue] = useState(null);
     // const onSelectGender = (itemValue) => {
     //     setSelectedValue(itemValue);
@@ -58,19 +99,19 @@ const PersonalDetails = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <ScrollView>
-            <View style={{ marginHorizontal: 20 }}>
-                <View style={styles.bar}>
-                    <View style={styles.darkBar} />
-                    <View style={styles.lightBar} />
-                </View>
-                <View>
-                    <Text style={styles.header}>What are your personal details? <Text style={{ color: 'red' }}>*</Text></Text>
-                    <Text style={styles.subheader}>Please provide your personal information</Text>
-                </View>
-                <View>
-                    {inputfunc('Legal first name', 'name-phone-pad', fName, setFName)}
-                    {inputfunc('Legal last name', 'name-phone-pad', lName, setLName)}
-                    {/* <DropDownPicker
+                <View style={{ marginHorizontal: 20 }}>
+                    <View style={styles.bar}>
+                        <View style={styles.darkBar} />
+                        <View style={styles.lightBar} />
+                    </View>
+                    <View>
+                        <Text style={styles.header}>What are your personal details? <Text style={{ color: 'red' }}>*</Text></Text>
+                        <Text style={styles.subheader}>Please provide your personal information</Text>
+                    </View>
+                    <View>
+                        {inputfunc('Legal first name', 'name-phone-pad', fName, setFName)}
+                        {inputfunc('Legal last name', 'name-phone-pad', lName, setLName)}
+                        {/* <DropDownPicker
                         items={[
                             { label: 'Gender', value: null },
                             { label: 'Female', value: 'female' },
@@ -80,7 +121,7 @@ const PersonalDetails = ({ navigation }) => {
                         defaultValue={selectedValue}
                         onChangeItem={onSelectGender}
                     /> */}
-                    {/* <Picker
+                        {/* <Picker
                         selectedValue={selectedValue}
                         onValueChange={onSelectGender}
                     >
@@ -90,26 +131,47 @@ const PersonalDetails = ({ navigation }) => {
                         <Picker.Item label="Not Specified" value="not_specified" />
                     </Picker> */}
 
-                    <ModalDropdown
-                        style={styles.input}
-                        options={genderOptions}
-                        // placeholder = 'Gender'
-                        defaultValue="Gender"
-                        onSelect={onSelectGender}
-                        value = {gender}
+                        <ModalDropdown
+                            style={styles.input}
+                            options={genderOptions}
+                            // placeholder = 'Gender'
+                            defaultValue="Gender"
+                            onSelect={onSelectGender}
+                            value={gender}
                         // onChangeText = {setGender}
-                    />
-                    {/* {inputfunc('Gender', 'name-phone-pad')} */}
-                    {inputfunc('Date of Birth DD/MM/YYYY', 'numbers-and-punctuation', DOB, setDOB)}
-                    {inputfunc('ID card / Passport number', 'numbers-and-punctuation', cardNum, setCardNum)}
+                        />
+                        {/* {inputfunc('Gender', 'name-phone-pad')} */}
+                        {inputfunc('Date of Birth DD/MM/YYYY', 'numbers-and-punctuation', DOB, setDOB)}
+                        {inputfunc('ID card / Passport number', 'numbers-and-punctuation', cardNum, setCardNum)}
+                        <View>
+                            <TouchableOpacity style={styles.btn} onPress={() => { showDatePicker() }}>
+                                <Text style={styles.btnText} >{selectedDate}</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.btn} onPress={() => { showTimePicker() }}>
+                                <Text style={styles.btnText} >{selectTime}</Text>
+                            </TouchableOpacity>
+                            <DateTimePickerModal
+                                isVisible={isDatePickerVisible}
+                                mode="date"
+                                onConfirm={handleDateConfirm}
+                                onCancel={hideDatePicker}
+                            />
+                            <DateTimePickerModal
+                                isVisible={isTimePickerVisible}
+                                mode="time"
+                                onConfirm={handleTimeConfirm}
+                                onCancel={hideTimePicker}
+                            />
+                        </View>
+                    </View>
+                    <View style={{ alignSelf: 'center' }}>
+                        {/* <CustomBtn onPress={() => navigation.navigate('Address Details', {firstName : fName, lastName : lName, Gender : selectedValue, DOB : DOB, CardNumber : cardNum})}> */}
+                        <CustomBtn onPress={() => navigation.navigate('Address Details')}>
+                            <ButtonText>NEXT</ButtonText>
+                        </CustomBtn>
+                        <View><Text /></View>
+                    </View>
                 </View>
-                <View style={{ alignSelf: 'center' }}>
-                    {/* <CustomBtn onPress={() => navigation.navigate('Address Details', {firstName : fName, lastName : lName, Gender : selectedValue, DOB : DOB, CardNumber : cardNum})}> */}
-                    <CustomBtn onPress={() => navigation.navigate('Address Details')}>
-                        <ButtonText>NEXT</ButtonText>
-                    </CustomBtn>
-                </View>
-            </View>
             </ScrollView>
         </View>
     )
@@ -166,28 +228,50 @@ export const styles = StyleSheet.create({
         fontSize: 18,
         paddingLeft: 20,
     },
-    image : {
-        height : 70,
-        width : 70,
-        borderRadius : 15,
+    image: {
+        height: 70,
+        width: 70,
+        borderRadius: 15,
     },
-    imageBtn : {
-        backgroundColor : '#b6e3d1',
-        height : 40,
-        width : '30%',
-        borderRadius : 8,
-        margin : 20
+    imageBtn: {
+        backgroundColor: '#b6e3d1',
+        height: 40,
+        width: '30%',
+        borderRadius: 8,
+        margin: 20
     },
-    imageBtnText : {
-        color : '#046e43',
-        fontSize : 14,
-        fontWeight : 600,
-        textAlign : 'center',
-        top : 8
+    imageBtnText: {
+        color: '#046e43',
+        fontSize: 14,
+        fontWeight: 600,
+        textAlign: 'center',
+        top: 8
     },
-    UploadImage : {
-        justifyContent : 'center',
-        alignItems : 'center',
-        marginVertical : 20,
+    UploadImage: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 20,
+    },
+
+    btn: {
+        width: '70%',
+        height: 50,
+        borderRadius: 15,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 2,
+        margin: 10
+    },
+
+    btnText: {
+        fontSize: 18,
+        fontWeight: '700',
+        color: 'black'
     }
+    // conContainer : {
+    //     flex : 1,
+    //     justifyContent : 'center',
+    //     alignItems : 'center'
+    // }
 })
